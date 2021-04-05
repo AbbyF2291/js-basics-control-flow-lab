f00fd59262b9a433e94bc3772925bd8976c92d01
AbbyFreund <abby_freund@hotmail.com>
2021-04-05 16:40:19 +1000

    Completed

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result
+  if (someNumber <= 400) {
+    result = 'This one is on me!'
+  }
+  if (someNumber > 2000) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  if (someNumber > 2500) {
+    result = 'No can do.'
+}
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result
+  if (city === 'NYC') {
+    result = 'Ok, sounds good.' ;
+  } else {
+    result = 'No go.';
+  }
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result
+  if (tip === 'generous') {
+    result = 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+    result = 'Thank you.';
+  } else {
+    result = 'Bye.'
+  }
+  return result
 }
